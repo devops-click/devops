f289007a5494b6fd29c135603bcfe9793f8dc5b8
Anderson Ayres <anfergainst@gmail.com>
2023-02-01 00:05:19 +0000

    Update 11-setup-nginx-wp.sh
    fix

--- configs/opt/devops/scripts/11-setup-nginx-wp.sh
+++ configs/opt/devops/scripts/11-setup-nginx-wp.sh
@@ -11,25 +11,27 @@
 #  https://devops.click | https://anderson.devops.click  |
 #--------------------------------------------------------|
 #       NGINX OPTIMIZED CONFIGURATION FOR WORDPRESS      |
-#                         v1.00                          |
+#                         v1.01                          |
 #--------------------------------------------------------'
 
 echo -e "** Downloading NGINX configurations..."
 [ -d /etc/letsencrypt ] && curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/letsencrypt/cli.ini -o /etc/letsencrypt/cli.ini
-[ -d /etc/nginx/snippets ] && mkdir -p /etc/nginx/snippets
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/conf.d/php-fpm.conf -o /etc/nginx/conf.d/php-fpm.conf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/default.d/php.conf -o /etc/nginx/default.d/php.conf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/snippets/converter-for-media.conf -o /etc/nginx/snippets/converter-for-media.conf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/snippets/letsencrypt-acme-challenge.conf -o /etc/nginx/snippets/letsencrypt-acme-challenge.conf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/snippets/wordpress-defaults.conf -o /etc/nginx/snippets/wordpress-defaults.conf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/fastcgi_params -o /etc/nginx/fastcgi_params
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/fastcgi.conf -o /etc/nginx/fastcgi.conf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/koi-utf -o /etc/nginx/koi-utf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/koi-win -o /etc/nginx/koi-win
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/mime.types -o /etc/nginx/mime.types
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/nginx.conf.template -o /etc/nginx/nginx.conf.template
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/scgi_params -o /etc/nginx/scgi_params
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/uwsgi_params -o /etc/nginx/uwsgi_params
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/win-utf -o /etc/nginx/win-utf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/php-fpm.d/www.conf -o /etc/php-fpm.d/www.conf
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/php.ini -o /etc/php.ini
+[ ! -d /etc/nginx/snippets ] && mkdir -p /etc/nginx/snippets
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/conf.d/php-fpm.conf -o /etc/nginx/conf.d/php-fpm.conf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/default.d/php.conf -o /etc/nginx/default.d/php.conf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/snippets/converter-for-media.conf -o /etc/nginx/snippets/converter-for-media.conf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/snippets/letsencrypt-acme-challenge.conf -o /etc/nginx/snippets/letsencrypt-acme-challenge.conf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/snippets/wordpress-defaults.conf -o /etc/nginx/snippets/wordpress-defaults.conf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/fastcgi_params -o /etc/nginx/fastcgi_params
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/fastcgi.conf -o /etc/nginx/fastcgi.conf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/koi-utf -o /etc/nginx/koi-utf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/koi-win -o /etc/nginx/koi-win
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/mime.types -o /etc/nginx/mime.types
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/nginx.conf.template -o /etc/nginx/nginx.conf.template
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/scgi_params -o /etc/nginx/scgi_params
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/uwsgi_params -o /etc/nginx/uwsgi_params
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/nginx/win-utf -o /etc/nginx/win-utf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/php-fpm.d/www.conf -o /etc/php-fpm.d/www.conf
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/php.ini -o /etc/php.ini
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/php.d/20-imagick.ini -o /etc/php.d/20-imagick.ini
+curl -L https://raw.githubusercontent.com/devops-click/devops/main/configs/wordpress/etc/php.d/20-zip.ini -o /etc/php.d/20-zip.ini
