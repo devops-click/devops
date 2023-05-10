bd627a22526d34cbe4cb90eb79e7fe90c20fb7e2
Anderson Ayres <anfergainst@gmail.com>
2023-05-10 10:38:14 +0100

    Update 00-setup-base.sh

--- configs/opt/devops/scripts/00-setup-base.sh
+++ configs/opt/devops/scripts/00-setup-base.sh
@@ -11,13 +11,13 @@
 #  https://devops.click | https://anderson.devops.click  |
 #--------------------------------------------------------|
 #                BASE DEVOPS.CLICK SCRIPTS               |
-#                         v1.00                          |
+#                         v1.01                          |
 #--------------------------------------------------------'
 
 echo -e "** Downloading BASE devops.click scripts and configurations..."
 mkdir -p /opt/devops/bin/
 mkdir -p /opt/devops/scripts/
-curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/opt/devops/bin/maintenance -o /opt/devops/bin/maintenance
+curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/opt/devops/bin/maintenance -H "Cache-Control: no-cache, no-store" -o /opt/devops/bin/maintenance
 curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/opt/devops/scripts/set-bash-colors-tput.sh -o /opt/devops/scripts/set-bash-colors-tput.sh
 curl -sL https://raw.githubusercontent.com/devops-click/devops/main/configs/opt/devops/scripts/set-bash-colors.sh -o /opt/devops/scripts/set-bash-colors.sh
 chmod +x -R /opt/devops/bin/*
